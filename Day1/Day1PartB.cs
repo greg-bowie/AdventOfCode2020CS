5b61dde8f4f01063b5d0097cea1e2e1ad8a31cb4
Greg Bowie <gb10aac>
2020-12-02 20:16:37 +0000

    Day 1 Part B Solution.

---
+++ Day1/Day1PartB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+    public class Day1PartB
+    {
+        public Tuple<int, int, int> GetNumbersSummingTo2020(List<int> inputList)
+        {
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                var remainder = 2020 - inputList[i];
+
+                for (int j = i + 1; j < inputList.Count; j++)
+                {
+                    if (inputList[j] < remainder)
+                    {
+                        var finalRemainder = remainder - inputList[j];
+
+                        if (inputList.Contains(finalRemainder))
+                        {
+                            return Tuple.Create(inputList[i], inputList[j], finalRemainder);
+                        }
+                    }
+                }
+            }
+            return Tuple.Create(0, 0, 0);
+        }
+    }
+}
---
+++ Day1Tests/Day1PartBTests.cs
@@ -0,0 +1,52 @@
+﻿using Day1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day1Tests
+{
+    [TestClass]
+    public class Day1PartBTests
+    {
+        [TestMethod]
+        public void Find3NumbersToSumTo2020_List()
+        {
+            List<int> inputList = new List<int>
+            {
+                1721,
+                979,
+                366,
+                299,
+                675,
+                1456
+            };
+
+            var sut = new Day1PartB();
+            var values = sut.GetNumbersSummingTo2020(inputList);
+
+            Assert.AreEqual(2020, values.Item1 + values.Item2 + values.Item3);
+            Assert.AreEqual(979, values.Item1);
+            Assert.AreEqual(366, values.Item2);
+            Assert.AreEqual(675, values.Item3);
+        }
+
+        [TestMethod]
+        public void Find3NumbersToSumTo2020_File()
+        {
+            var filename = @"C:\Users\jock_\source\repos\AdventOfCode2020CS\Day1\input.txt";
+            var lines = File.ReadAllLines(filename);
+            var inputList = lines.Select(int.Parse).ToList();
+
+            var sut = new Day1PartB();
+            var values = sut.GetNumbersSummingTo2020(inputList);
+
+            Assert.AreEqual(2020, values.Item1 + values.Item2 + values.Item3);
+            Assert.AreEqual(591, values.Item1);
+            Assert.AreEqual(1021, values.Item2);
+            Assert.AreEqual(408, values.Item3);
+        }
+    }
+}
